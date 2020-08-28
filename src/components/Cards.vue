abc5651f1b9f8e49bc009b97696cb5a459624086
rabbuttz <49971163+rabbuttz@users.noreply.github.com>
2020-08-28 09:21:31 +0900

    記事に接続できるようになった

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="cards">
-    <v-card max-width="80%" class="mx-auto">
-      <v-container>
-        <v-row dense>
-          <v-col cols="14">
-            <v-card color="grey" @click.stop="dialog=true" dark>
-              <v-card-title class="headline">むにょわBlogってなんだ</v-card-title>
-              <v-card-subtitle>押しても何もねえから</v-card-subtitle>
-              <v-card-subtitle>投稿日 2020/08/26</v-card-subtitle>
-            </v-card>
-          </v-col>
-        </v-row>
-        <v-row dense>
-          <v-col cols="14">
-            <v-card color="red" @click.stop="dialog=true" dark>
-              <v-card-title class="headline">ここにいろんな記事</v-card-title>
-              <v-card-subtitle>投稿していく</v-card-subtitle>
-              <v-card-subtitle>投稿日 2020/08/26</v-card-subtitle>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
+    <v-app>
+      <br />
+      <v-card color="yellow" max-width="80%" class="mx-auto">
+        <v-container>
+          <v-row class="text-center">
+            <v-col class="mb-2" dark>
+              <h1 class="display-2 font-weight-bold mb-3">むにょわBlog</h1>
+            </v-col>
+          </v-row>
+          <v-row dense v-for="article in articles" v-bind:key="article.id">
+            <v-col cols="14">
+              <v-card color="white" @click="gotoarticle">
+                <v-card-title class="headline">{{article.title}}</v-card-title>
+                <v-card-subtitle>{{article.description}}</v-card-subtitle>
+                <v-card-subtitle>投稿日{{article.date}}</v-card-subtitle>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-app>
 
     <!-- ダイアログ表示 -->
     <v-dialog v-model="dialog" scrollable max-width="80%">
@@ -38,20 +37,46 @@
 </template>
 
 <script>
+import router from "../router";
+
 export default {
   data() {
     return {
       dialog: false,
+      articles: [
+        {
+          id: 1,
+          title: "むにょわBlogってなんだよ",
+          description:
+            "令和2年現在、人類の持続的な進化に於いてむにょわは欠かせない概念となってお…",
+          date: "2020/08/27",
+        },
+        {
+          id: 2,
+          title: "あむさん is 誰",
+          description:
+            "あむさんはポスト安倍晋三の第一候補として知られており、人望が厚く、とてもずっちーことが知ら…",
+          date: "2020/08/27",
+        },
+        {
+          id: 3,
+          title: "aaaaaaaaaaaaaaaa",
+          description:
+            "令和2年現在、人類の持続的な進化に於いてむにょわは欠かせない概念となってお…",
+          date: "2020/08/27",
+        },
+      ],
     };
   },
   methods: {
+    gotoarticle() {
+      router.push("Article");
+    },
     twitterShare() {
       //シェアする画面を設定
       this.dialog = false;
       var shareURL =
         "https://twitter.com/intent/tweet?text=" +
-        "@2a4g5j7p お兄ちゃん許して…😢\n" +
-        "#あむのゴミ箱（開発中） " +
         "&url=" +
         "https://relaxed-bassi-cfd91c.netlify.app/";
       //シェア用の画面へ移行
